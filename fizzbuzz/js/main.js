5d670ab62157f12c6f0b7ce3d401168a680d39ed
CaldatoLuca <lucacaldato15@gmail.com>
2024-03-24 16:11:51 +0100

    feat: fizzbuzz

---
+++ fizzbuzz/js/main.js
@@ -0,0 +1,38 @@
+"use.strict";
+
+// ?elementi HTML
+const elementLista = document.querySelector(".-lc-list");
+
+// ?variabili
+const divisibilePerTre = "Fizz";
+const divisibilePerCinque = "Buzz";
+
+for (let i = 1; i <= 100; i++) {
+  //* creazione elemento html li
+  const li = document.createElement("li");
+  li.classList.add("-lc-list-item");
+  li.classList.add("rounded");
+  li.classList.add("shadow-lg");
+
+  //! condizioni
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(divisibilePerTre + divisibilePerCinque);
+    li.append(divisibilePerTre + divisibilePerCinque);
+    li.classList.add("-lc-number-3-5");
+  } else if (i % 5 === 0) {
+    console.log(divisibilePerCinque);
+    li.append(divisibilePerCinque);
+    li.classList.add("-lc-number-5");
+  } else if (i % 3 === 0) {
+    console.log(divisibilePerTre);
+    li.append(divisibilePerTre);
+    li.classList.add("-lc-number-3");
+  } else {
+    console.log({ i });
+    li.append(i);
+    li.classList.add("-lc-number");
+  }
+
+  //* inserirsco li nella ul
+  elementLista.append(li);
+}
